eccdd08c008d7fc3b8b1b49833ca6cc0d771a3f3
ninggc <ninggc@foxmail.com>
2018-02-11 14:10:57 +0800

    auto-update

---
+++ auto_git.bat
@@ -0,0 +1,14 @@
+echo off
+git add .
+echo;
+echo ============ add end ============
+echo;
+git commit -m "auto-update"
+echo;
+echo ============ commit end ============
+echo;
+git push origin
+echo;
+echo ============ push end ============
+echo;
+pause
